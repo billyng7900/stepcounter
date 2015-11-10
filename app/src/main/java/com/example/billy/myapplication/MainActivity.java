c23bab260fd1c209cad8e86ffc4111e665baa5fa
billyng7900 <billyng7900@hotmail.com>
2015-11-10 23:32:14 +0800

    added test

--- app/src/main/java/com/example/billy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/billy/myapplication/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends Activity implements SensorEventListener{
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
             if (serviceClass.getName().equals(service.service.getClassName())) {
+                //check it is here
                 return true;
             }
         }
