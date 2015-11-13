c5fda956b6503a960cf030467aac261a653957c4
Hing <waihingli3@gmail.com>
2015-11-14 01:32:46 +0800

    Fitness Plan~ (calculate BMI)

--- app/src/main/java/com/example/billy/myapplication/FitnessActivity.java
+++ app/src/main/java/com/example/billy/myapplication/FitnessActivity.java
@@ -2,16 +2,24 @@ package com.example.billy.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.billy.myapplication.R;
 
 /**
  * Created by WaiHing on 13/11/2015.
  */
-public class Fitness extends Activity {
+public class FitnessActivity extends Activity {
+
+    TextView tv_BMI;
+    public FitnessPlan fitness;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inifitness);
+        tv_BMI = (TextView)findViewById(R.id.tv_bmistatus);
+        fitness = new FitnessPlan();
+        tv_BMI.setText("BMI = " + String.format("%.1f",fitness.getBMI()) + "\nYou are in " + fitness.getBMIStatus());
     }
 }
