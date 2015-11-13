0640d556c8b199f0d39db68008b78e6a623a6801
Hing <waihingli3@gmail.com>
2015-11-13 23:57:44 +0800

    hi test add new activity

---
+++ app/src/main/java/com/example/billy/myapplication/Fitness.java
@@ -0,0 +1,17 @@
+package com.example.billy.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.billy.myapplication.R;
+
+/**
+ * Created by WaiHing on 13/11/2015.
+ */
+public class Fitness extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inifitness);
+    }
+}
