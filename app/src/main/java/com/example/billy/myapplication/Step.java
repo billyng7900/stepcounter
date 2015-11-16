17ab96f459bd0515dd63f0b8325d16bcac74cd39
billyng7900 <billyng7900@hotmail.com>
2015-11-16 23:01:29 +0800

    implemented chart step

--- app/src/main/java/com/example/billy/myapplication/Step.java
+++ app/src/main/java/com/example/billy/myapplication/Step.java
@@ -1,9 +1,15 @@
 package com.example.billy.myapplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by Billy on 14/11/2015.
  */
 public class Step {
+    private int step;
+    private String date;
     public int getStep() {
         return step;
     }
@@ -20,7 +26,19 @@ public class Step {
         this.date = date;
     }
 
-    private int step;
-    private String date;
+    public Date getConvertToDate()
+    {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date convertedDate = new Date();
+        try{
+            convertedDate = dateFormat.parse(date);
+            return convertedDate;
+        }catch(Exception e)
+        {
+            return null;
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/example/billy/myapplication/StepChartActivity.java
+++ app/src/main/java/com/example/billy/myapplication/StepChartActivity.java
@@ -1,21 +1,87 @@
 package com.example.billy.myapplication;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.view.MotionEvent;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
-public class StepChartActivity extends Activity {
-    private LineChart mChart;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class StepChartActivity extends Activity implements OnChartGestureListener, OnChartValueSelectedListener{
+    private LineChart lineChart;
+    private SharedPreferences settings;
+    private int targetStep;
+    StepDbHelper dbHelper;
+    ArrayList<Step> stepList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_chart_avaity);
-        mChart = (LineChart)findViewById(R.id.chart);
+        lineChart = (LineChart)findViewById(R.id.chart);
+        dbHelper = new StepDbHelper(this);
+        settings = getSharedPreferences("fitness_plan", MODE_PRIVATE);
+        targetStep = settings.getInt("targetStepDay",0);
+        lineChart.setOnChartGestureListener(this);
+        lineChart.setOnChartValueSelectedListener(this);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription("");
+        lineChart.setNoDataText("No Step Record.");
+        lineChart.setTouchEnabled(true);
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+        lineChart.setPinchZoom(true);
+        //lineChart.setBackgroundColor(Color.GRAY);
+
+        XAxis xaxis = lineChart.getXAxis();
+        //Typeface tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+        /*LimitLine llx = new LimitLine(-30f,"Lower Limit");
+        llx.setLineWidth(4f);
+        llx.enableDashedLine(10f, 10f, 0f);
+        llx.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        llx.setTextSize(10f);
+        llx.setTypeface(tf);*/
+        LimitLine llx_upper = new LimitLine(targetStep,"Step target");
+        llx_upper.setLineWidth(4f);
+        llx_upper.enableDashedLine(10f, 10f, 0f);
+        llx_upper.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        llx_upper.setTextSize(10f);
+        //llx_upper.setTypeface(tf);
+        YAxis yAxis = lineChart.getAxisLeft();
+        yAxis.removeAllLimitLines();
+        yAxis.addLimitLine(llx_upper);
+        yAxis.setAxisMaxValue(12000f);
+        yAxis.setAxisMinValue(0f);
+        yAxis.setStartAtZero(true);
+        yAxis.enableGridDashedLine(10f, 10f, 0f);
+        yAxis.setDrawLimitLinesBehindData(true);
+        lineChart.getAxisRight().setEnabled(false);
+        setData();
+        lineChart.animateX(2500, Easing.EasingOption.EaseInOutQuart);
+        Legend legend = lineChart.getLegend();
+        legend.setForm(Legend.LegendForm.LINE);
     }
 
     @Override
@@ -38,5 +104,146 @@ public class StepChartActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void insertRecord(int s,String date)
+    {
+        //test function!!!!! delete it before launch!
+        ContentValues upValues = new ContentValues();
+        upValues.put(StepEntry.COLUMN_NAME_Date, date);
+        upValues.put(StepEntry.COLUMN_NAME_Step, s);
+        dbHelper.insertStep(upValues,this);
+
+    }
+    private void setData()
+    {
+        /*for(int i=14;i>0;i--)
+        {
+        //gen November step
+            int genStep = (int)(Math.random()*10000);
+            insertRecord(genStep,"2015-11-"+i);
+        }*/
+        /*for(int i=30;i>0;i--)
+        {
+            //gen October step
+            int genStep = (int)(Math.random()*10000);
+            insertRecord(genStep,"2015-10-"+i);
+        }*/
+        stepList = dbHelper.getAllStepRecord(this);
+        if(stepList.size()>30)
+        {
+            stepList = getLatest30DaysResut();
+        }
+        ArrayList<String> xVals = new ArrayList<>();
+        for(int i=0;i<stepList.size();i++)
+        {
+            xVals.add("Day "+(i+1));
+        }
+        ArrayList<Entry> yVals = new ArrayList<>();
+        for(int i=0;i<stepList.size();i++)
+        {
+            int val = stepList.get(i).getStep();
+            yVals.add(new Entry(val,i));
+        }
+        LineDataSet set = new LineDataSet(yVals,"Step Count Set");
+        set.enableDashedLine(10f, 5f, 0f);
+        set.enableDashedHighlightLine(10f, 5f, 0f);
+        set.setColor(Color.BLACK);
+        set.setCircleColor(Color.BLACK);
+        set.setLineWidth(1f);
+        set.setCircleSize(2f);
+        set.setDrawCircleHole(false);
+        set.setValueTextSize(9f);
+        set.setFillAlpha(65);
+        set.setFillColor(Color.BLACK);
+        ArrayList<LineDataSet> finalData = new ArrayList<>();
+        finalData.add(set);
+        LineData data = new LineData(xVals,finalData);
+        lineChart.setData(data);
+    }
+
+    private ArrayList<Step> getLatest30DaysResut()
+    {
+        ArrayList<Step> newStepList = new ArrayList<>();
+        Date maxDate = stepList.get(0).getConvertToDate();
+        for(Step s:stepList)
+        {
+            int result = maxDate.compareTo(s.getConvertToDate());
+            if(result<0)
+                maxDate = s.getConvertToDate();
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(maxDate);
+        cal.add(Calendar.DATE,-30);
+        Date date30Before = cal.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateBefore30Days = dateFormat.format(date30Before);
+        Date finalDate = new Date();
+        try {
+            finalDate = dateFormat.parse(dateBefore30Days);
+        }catch (Exception e)
+        {
+
+        }
+
+        for(Step s:stepList)
+        {
+            int result = finalDate.compareTo(s.getConvertToDate());
+            if(result<1)
+            {
+                newStepList.add(s);
+            }
+        }
+        return newStepList;
+    }
+
+    @Override
+    public void onChartGestureStart(MotionEvent motionEvent, ChartTouchListener.ChartGesture chartGesture) {
+
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent motionEvent, ChartTouchListener.ChartGesture chartGesture) {
+
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public void onChartFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+
+    }
+
+    @Override
+    public void onChartScale(MotionEvent motionEvent, float v, float v1) {
+
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent motionEvent, float v, float v1) {
+
+    }
+
+    @Override
+    public void onValueSelected(Entry entry, int i, Highlight highlight) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
     }
 }
